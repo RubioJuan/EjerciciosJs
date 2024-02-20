230d7f84c84419ee3e07712b68fafbcdb90d0408
Juan Felipe Rubio Sanabria <152001380+RubioJuan@users.noreply.github.com>
2024-02-20 06:58:44 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-funtion ( n ===1 )
+// 1er Ejercicio
+
+// Construya una expresión regular que permita validar el Nit de una empresa en Colombia.
+
+const NIT = 123
+
+let zNit = Number(prompt("Ingrese el Nit de la empresa: "))
+
+function validarNit (){
+    if (zNit === NIT){
+        alert("Eres el mejor XD")
+    }
+    else{
+        alert("Acuestese Zorra")
+    }
+}
+
+validarNit();
