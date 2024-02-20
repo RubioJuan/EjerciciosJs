7d577f726f47ceb25e085d4c351ac99dd4d29c8f
Juan Felipe Rubio Sanabria <152001380+RubioJuan@users.noreply.github.com>
2024-02-20 13:54:21 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,17 +2,28 @@
 
 // Construya una expresión regular que permita validar el Nit de una empresa en Colombia.
 
-const NIT = 123
+// const NIT = 123
 
-let zNit = Number(prompt("Ingrese el Nit de la empresa: "))
+// let zNit = Number(prompt("Ingrese el Nit de la empresa: "))
 
-function validarNit (){
-    if (zNit === NIT){
-        alert("Eres el mejor XD")
-    }
-    else{
-        alert("Acuestese Zorra")
-    }
+
+// function validarNit (){
+//     if (zNit === NIT){
+//         alert("Aprobado")
+//     }
+//     else{
+//         alert("Los siento, no ha sido admitido, vuelve a intentarlo")
+//     }
+// }
+
+// validarNit();
+
+const regexNIT = /\d.0-9/g;
+
+const nit = "123.456.789-0";
+
+if (regexNIT.test(nit)) {
+    console.log("El NIT es válido.");
+} else {
+    console.log("El NIT no es válido.");
 }
-
-validarNit();
